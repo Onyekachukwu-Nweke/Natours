37cfe46ee380ed1458273d6abbc675ad67bf86e8
Onyekachukwu-Nweke <nwekeejioforscheller@gmail.com>
2023-10-29 20:34:00 +0100

    feat: configure server js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
+const dotenv = require('dotenv')
 const app = require('./app');
 
-console.log(app.get('env'));
+dotenv.config({ path: './config.env'});
+console.log(process.env);
 
 const port = 3000;
 app.listen(port, () => {
