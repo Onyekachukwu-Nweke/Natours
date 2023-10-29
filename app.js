799054073ef6309fe3cf72c95188c14824e2da69
Onyekachukwu-Nweke <nwekeejioforscheller@gmail.com>
2023-10-29 19:00:04 +0100

    fix: File restructuring

---
+++ app.js
@@ -0,0 +1,33 @@
+const fs = require('fs')
+const express = require('express');
+const morgan = require('morgan');
+
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
+
+const app = express();
+
+// 1. Middlewares
+
+app.use(morgan('dev'));
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log('Hello From the middleware');
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+})
+
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+
+// 4. Server
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`App running on port ${port}`)
+});
