761bab07ef09614f1e8b438a3522b8683ce3d32c
Onyekachukwu-Nweke <nwekeejioforscheller@gmail.com>
2023-11-04 01:01:14 +0100

    feat: Setting up DB config

--- app.js
+++ app.js
@@ -7,8 +7,10 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // 1. Middlewares
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
-app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
@@ -20,7 +22,7 @@ app.use((req, res, next) => {
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
-})
+});
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
