a8dfaae66238cd0578296bb292ebc3a76f4fa5e4
Onyekachukwu-Nweke <nwekeejioforscheller@gmail.com>
2023-11-10 19:46:31 +0100

    feat: Implemented and tested pagination on tour controller

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -32,6 +32,24 @@ exports.getAllTours = async (req, res) => {
     }
 
     // 3) Field Limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
+
+    // 4.) Pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This page does not exist');
+    }
 
     // Execute Query
     const tours = await query;
