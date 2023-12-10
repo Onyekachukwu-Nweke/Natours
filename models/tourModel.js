794974f0be540dcc96d443bebe80aaccc5bbbf03
Onyekachukwu-Nweke <nwekeejioforscheller@gmail.com>
2023-12-10 19:27:14 +0100

    feat: Geospatial Aggregation - Calculating Distances

--- models/tourModel.js
+++ models/tourModel.js
@@ -184,14 +184,14 @@ tourSchema.pre(/^find/, function (next) {
 // });
 
 // Aggregation Middleware
-tourSchema.pre('aggregate', function (next) {
-  this.pipeline().unshift({
-    $match: { secretTour: { $ne: true } },
-  });
+// tourSchema.pre('aggregate', function (next) {
+//   this.pipeline().unshift({
+//     $match: { secretTour: { $ne: true } },
+//   });
 
-  console.log(this.pipeline());
-  next();
-});
+//   console.log(this.pipeline());
+//   next();
+// });
 
 const Tour = mongoose.model('Tour', tourSchema);
 
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -9,6 +9,7 @@ const {
   getTourStats,
   getMonthlyPlan,
   getToursWithin,
+  getDistances,
 } = require('../controllers/tourController');
 const { protect, restrictTo } = require('../controllers/authController');
 const reviewRouter = require('./reviewRoutes');
@@ -40,6 +41,8 @@ router
   .get(getToursWithin);
 // /tours-within?distance=233,center=-40,45&unit=mi
 // /tours-within/233/center/-40,45/unit/mi
+
+router.route('/distances/:latlng/unit/:unit').get(getDistances);
 
 router
   .route('/')
