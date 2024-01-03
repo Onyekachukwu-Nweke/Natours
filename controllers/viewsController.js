c5d9e5ae4df4928e6980ff9b1483973a15419741
Onyekachukwu-Nweke <nwekeejioforscheller@gmail.com>
2024-01-03 23:05:36 +0100

    feat: building the tour page and fix: asset paths

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,5 +1,7 @@
 const Tour = require('../models/tourModel');
+const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
 
 exports.getOverview = catchAsync(async (req, res) => {
   // 1) Get tour data from collection
@@ -14,8 +16,20 @@ exports.getOverview = catchAsync(async (req, res) => {
   });
 });
 
-exports.getTour = (req, res) => {
-  res.status(200).render('tour', {
-    title: 'The Forest Hiker',
+exports.getTour = catchAsync(async (req, res) => {
+  // 1. Get the data, for that particular tour (including reviews and guides)
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'review ratings user',
   });
-};
+
+  // if (!tour) {
+  //   return next(new AppError('There is no tour with that name.', 404));
+  // }
+  // 2. Build template
+  // 3. Render template using data from 1.
+  res.status(200).render('tour', {
+    title: `${tour.name} tour`,
+    tour,
+  });
+});
