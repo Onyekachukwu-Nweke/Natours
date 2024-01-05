2e399c710da29bf1f7d3a17b737d78a877f352e3
Onyekachukwu-Nweke <nwekeejioforscheller@gmail.com>
2024-01-05 12:46:58 +0100

    feat: Integrating log in api

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
+const cookieParser = require('cookie-parser');
 const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
@@ -50,6 +51,7 @@ app.use('/api', limiter);
 
 // Body Parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
+app.use(cookieParser());
 
 // Data sanitization against NoSQL query injection
 app.use(mongoSanitize());
@@ -78,7 +80,7 @@ app.use(
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
-  // console.log(req.headers);
+  console.log(req.headers);
   next();
 });
 
